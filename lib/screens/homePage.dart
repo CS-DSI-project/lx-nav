09799bb297c052a0b3bcc46d86dd4cd4b68639ba
flukerbooker <win.vorapol52@gmail.com>
2020-04-18 16:25:01 +0700

    [UPDATED] re-design homepage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -25,82 +25,107 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedPage = 0;
+  final _pageOptions = [
+    Text('Home'),
+    Text('Search'),
+    Text("Map"),
+    Text('Contact')
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('NavKMUTT'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: 350.0,
-              child: FlutterMap(
-                  options:
-                  MapOptions(center: LatLng(13.652094, 100.494061), zoom: 17),
-                  layers: [
-                    TileLayerOptions(
-                      urlTemplate:
-                      'https://api.mapbox.com/styles/v1/flukerbooker/ck8wr4jrm3k5g1io1dbeuun5n/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmx1a2VyYm9va2VyIiwiYSI6ImNrOG43NnJ5YTA5YnIzZ21lMGUzdXFoMmMifQ.2a9t5SV3aSUqBm28Qc13bw',
-                      additionalOptions: {
-                        'accessToken : pk.eyJ1IjoiZmx1a2VyYm9va2VyIiwiYSI6ImNrOG43NnJ5YTA5YnIzZ21lMGUzdXFoMmMifQ.2a9t5SV3aSUqBm28Qc13bw'
-                            'id': 'mapbox.mapbox-streets-v7'
-                      },
-                    )
-                  ]
-              ),
+      appBar: AppBar(
+        title: Text('NavKMUTT'),
+      ),
+      body: SafeArea(
+        child: _pageOptions[_selectedPage],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            boxShadow: [BoxShadow(color: Colors.black54, blurRadius: 2)]),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedPage,
+          elevation: 20.0,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
             ),
-            Center(
-                child: showButton(context)
-            )
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Search'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contacts),
+              title: Text('Contact'),
+            ),
           ],
-        )
+        ),
+      ),
     );
   }
 }
 
-Widget showButton(context) {
-  return Container(
-    padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        roomPageButton(context),
-        errorPageButton(context),
-        secureButton(context)
-      ],
-    ),
-  );
-}
-
-Widget roomPageButton(context) {
-  return RaisedButton(
-    child: Text('Room Page'),
-    onPressed: () {
-      MaterialPageRoute materialPageRoute =
-          MaterialPageRoute(builder: (BuildContext context) => Room());
-      Navigator.of(context).push(materialPageRoute);
-    },
-  );
-}
-
-Widget errorPageButton(context) {
-  return RaisedButton(
-    child: Text('Error Page'),
-    onPressed: () {
-      MaterialPageRoute materialPageRoute =
-          MaterialPageRoute(builder: (BuildContext context) => Error());
-      Navigator.of(context).push(materialPageRoute);
-    },
-  );
-}
-
-Widget secureButton(context) {
-  return RaisedButton(
-    child: Text('Secure Do not touch it do not work'),
-    onPressed: () {
-//      MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext context) => ItemsWidget());
+//Widget showButton(context) {
+//  return Container(
+//      child: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          crossAxisAlignment: CrossAxisAlignment.center,
+//          children: <Widget>[
+//            roomPageButton(context),
+//            errorPageButton(context),
+//        ],
+//      ),
+//    ),
+//  );
+//}
+//
+//Widget roomPageButton(context) {
+//  return RaisedButton(
+//    child: Text('Room Page'),
+//    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+//    padding: const EdgeInsets.all(20.0),
+//    onPressed: () {
+//      MaterialPageRoute materialPageRoute =
+//          MaterialPageRoute(builder: (BuildContext context) => Room());
 //      Navigator.of(context).push(materialPageRoute);
-    },
-  );
-}
+//    },
+//  );
+//}
+//
+//Widget errorPageButton(context) {
+//  return RaisedButton(
+//    child: Text('Error Page'),
+//    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+//    padding: const EdgeInsets.all(20.0),
+//    onPressed: () {
+//      MaterialPageRoute materialPageRoute =
+//          MaterialPageRoute(builder: (BuildContext context) => Error());
+//      Navigator.of(context).push(materialPageRoute);
+//    },
+//  );
+//}
+//
+//Widget secureButton(context) {
+//  return RaisedButton(
+//    child: Text('Secure Do not touch it do not work', softWrap: true),
+//    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+//    padding: const EdgeInsets.all(20.0),
+//    onPressed: () {
+////      MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext context) => ItemsWidget());
+////      Navigator.of(context).push(materialPageRoute);
+//    },
+//  );
+//}
