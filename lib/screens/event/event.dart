600be47cf4efed60faa32dd7eacf27992efbd7b5
flukerbooker <win.vorapol52@gmail.com>
2020-05-02 12:02:25 +0700

    [UPDATED] Final Event

--- lib/screens/event/event.dart
+++ lib/screens/event/event.dart
@@ -8,7 +8,8 @@ class Event extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (name.length > 22) {
-      return new Scaffold(body: NestedScrollView(
+      return new Scaffold(
+          body: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
             SliverMultilineAppBar(
@@ -30,21 +31,26 @@ class Event extends StatelessWidget {
       ));
     } else {
       return new Scaffold(
-        appBar: AppBar(
-          title: Text(name),
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.more_vert),
-            ),
-          ],
-        ),
+          body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text(name),
+              actions: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_vert),
+                ),
+              ],
+            )
+          ];
+        },
         body: ListDetail(),
-      );
+      ));
     }
   }
 }
@@ -98,87 +104,90 @@ class ListItemWidget extends State<ListDetail> {
   Widget build(BuildContext context) {
     return Container(
         child: ListView.builder(
+            padding: EdgeInsets.all(0.0),
             itemCount: items.length,
             itemBuilder: (context, index) {
-              return Container(
-                  height: 120,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        height: 120.0,
-                        width: 100.0,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(5),
-                                topLeft: Radius.circular(5)),
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage("images/LxRoom1.png"))),
-                      ),
-                      Container(
-                        height: 200,
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(10, 8, 0, 0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                items[index],
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                                child: Container(
-                                  child: Text(
-                                    "Business Conference 2020",
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.w500),
-                                  ),
+              return Card(
+                  elevation: 5,
+                  child: Container(
+                    height: 120,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          height: 120.0,
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(5),
+                                  topLeft: Radius.circular(5)),
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage("images/LxRoom1.png"))),
+                        ),
+                        Container(
+                          height: 200,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(10, 8, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  items[index],
                                 ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
-                                child: Container(
-                                  child: Text(
-                                    "Time: 10.00 - 15.00",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w400,
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                                  child: Container(
+                                    child: Text(
+                                      "Business Conference 2020",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                  padding: EdgeInsets.fromLTRB(0, 2, 0, 2),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: <Widget>[
-                                      RaisedButton(
-                                        color: Colors.orange,
-                                        onPressed: () {
-                                          Navigator.of(context).push(
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      EventDetail(items[index]
-                                                              .toString() +
-                                                          ' Event')));
-                                        },
-                                        child: const Text(
-                                          'Show more',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              fontWeight: FontWeight.w400),
-                                        ),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                                  child: Container(
+                                    child: Text(
+                                      "Time: 10.00 - 15.00",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w400,
                                       ),
-                                    ],
-                                  ))
-                            ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 2, 0, 2),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        RaisedButton(
+                                          color: Colors.orange,
+                                          onPressed: () {
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        EventDetail(items[index]
+                                                                .toString() +
+                                                            ' Event')));
+                                          },
+                                          child: const Text(
+                                            'Show more',
+                                            style: TextStyle(
+                                                fontSize: 13,
+                                                fontWeight: FontWeight.w400),
+                                          ),
+                                        ),
+                                      ],
+                                    ))
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                );
+                        )
+                      ],
+                    ),
+                  ));
             }));
   }
 
